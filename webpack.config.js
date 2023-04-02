a6890d12909908dcf32896e71f91dc10f1baa825
Clymoplet <jahnnho.moraga@gmail.com>
2023-04-02 13:24:08 -0400

    [FIX PUBLIC PATH]

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,9 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/",
+    publicPath: "./",
+    
+
   },
   resolve: {
     extensions: [".js", ".jsx"],
